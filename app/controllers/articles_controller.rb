93c64f09df4034485ebbd88a3beb6057994b2e15
Kant Sunthad <kant@Kants-MacBook-Air.local>
2020-03-13 14:25:05 +0700

    make app production all

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,6 +6,12 @@ class ArticlesController < ApplicationController
     def show
         @article = Article.find(params[:id])
     end
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        redirect_to articles_path
+
+    end
     def edit
         @article = Article.find(params[:id])
     end
